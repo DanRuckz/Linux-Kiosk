ab53835dc8625ddb9efa93a70f57f57b0a83503a
DanRuckz <dan210993@hotmail.com>
2022-04-01 14:57:25 +0300

    fixed spacing

--- kiosk.cpp
+++ kiosk.cpp
@@ -30,66 +30,66 @@ int main(int argc, char *argv[]){
     signal(SIGTERM, handle_signal);
     signal(SIGUSR1, handle_signal);
         
-        //Checks if the number of arguments is sufficient, to avoid sending a vector without arguments to execv. One permanent element is always a NULL pointer, because the execv requires the argument vector to be NULL
-        //terminated for each argument, and the last argument must be a null pointer. Writing the error message to stdout and quitting.
-        if(command.size() <2){
+    //Checks if the number of arguments is sufficient, to avoid sending a vector without arguments to execv. One permanent element is always a NULL pointer, because the execv requires the argument vector to be NULL
+    //terminated for each argument, and the last argument must be a null pointer. Writing the error message to stdout and quitting.
+    if(command.size() <2){
         write(STDOUT_FILENO, fail_arguments, sizeof(fail_arguments));
         return 0;
-        }
-        //This is where we prepare the strings for the command to look up the process name and preemtively kill it, so the process the kiosk spawns should be the only one.
-        //All of it is done by just simply running shell commands and getting their stdout back to a variable.
-        std::string current_program_name = "echo " + std::string(command[0]) + "|awk -F/ '{print $NF}'"; //getting the program name using shell commands
-        current_program_name = execCommand(current_program_name.c_str());
-        current_program_name[strlen(current_program_name.c_str())-1] = ' '; //The command returned has a \n in the end, the current line replaces it with a ' '.
-        std::string kill_command = "ps aux |grep -i " + current_program_name + "|grep -v 'grep'|grep -v "+ argv[0] + "|awk '{print $2}' |xargs kill 2>/dev/null";
-        std::string output = execCommand((char *)kill_command.c_str()); //this executes the following ps aux |grep -i <kiosk_program_name> |grep -v 'grep'|grep -v <current_program_name> |awk '{print $2}' |xargs kill 2>/dev/null"
+    }
+    //This is where we prepare the strings for the command to look up the process name and preemtively kill it, so the process the kiosk spawns should be the only one.
+    //All of it is done by just simply running shell commands and getting their stdout back to a variable.
+    std::string current_program_name = "echo " + std::string(command[0]) + "|awk -F/ '{print $NF}'"; //getting the program name using shell commands
+    current_program_name = execCommand(current_program_name.c_str());
+    current_program_name[strlen(current_program_name.c_str())-1] = ' '; //The command returned has a \n in the end, the current line replaces it with a ' '.
+    std::string kill_command = "ps aux |grep -i " + current_program_name + "|grep -v 'grep'|grep -v "+ argv[0] + "|awk '{print $2}' |xargs kill 2>/dev/null";
+    std::string output = execCommand((char *)kill_command.c_str()); //this executes the following ps aux |grep -i <kiosk_program_name> |grep -v 'grep'|grep -v <current_program_name> |awk '{print $2}' |xargs kill 2>/dev/null"
 
-        //Initializing needed variables for the exis status of the child process and the timer with the fork count
-        int child_status_val;
-        time_t start_time = time(0);
-        unsigned int number_of_forks = 0;
-        time_t running_time;
+    //Initializing needed variables for the exis status of the child process and the timer with the fork count
+    int child_status_val;
+    time_t start_time = time(0);
+    unsigned int number_of_forks = 0;
+    time_t running_time;
         
-        //Creating a pipe here for the processes to communicate
-        int fd[2];
-        if(pipe(fd) == -1){
-            perror("Failed");
-            return -1;
-        }
-        //This is the loop where the kiosk is happening. A fork is happening here, the child is running and the parent is waiting
-        //as soon as the child has died, the parent would print "finished waiting, closing." and get back to the beginning of the loop and open a new child process with the same command.
-        //The aim is to keep the target window open
-        while(true){
+    //Creating a pipe here for the processes to communicate
+    int fd[2];
+    if(pipe(fd) == -1){
+        perror("Failed");
+        return -1;
+    }
+    //This is the loop where the kiosk is happening. A fork is happening here, the child is running and the parent is waiting
+    //as soon as the child has died, the parent would print "finished waiting, closing." and get back to the beginning of the loop and open a new child process with the same command.
+    //The aim is to keep the target window open
+    while(true){
             
-            //The following 2 if statements are responsible to make sure that the program doesn't get into an infinite fork loop
-            //Sometimes the target programs return 0 after opening and they don't close - this creates an infinite loop of opening target programs and avoids a total meltdown
-            //It's based on a timer and will stop execution is the child process spawns fast enough, this is good enough for a browser.
-            running_time = time(0);
-            if(running_time - start_time <= 2 && number_of_forks >= 2){
-                handle_signal(SIGUSR1);
-            }
-            if(running_time - start_time > 2 && number_of_forks <2){
-                start_time = time(0);
-                number_of_forks = 0;
-            }
+        //The following 2 if statements are responsible to make sure that the program doesn't get into an infinite fork loop
+        //Sometimes the target programs return 0 after opening and they don't close - this creates an infinite loop of opening target programs and avoids a total meltdown
+        //It's based on a timer and will stop execution is the child process spawns fast enough, this is good enough for a browser.
+        running_time = time(0);
+        if(running_time - start_time <= 2 && number_of_forks >= 2){
+            handle_signal(SIGUSR1);
+        }
+        if(running_time - start_time > 2 && number_of_forks <2){
+            start_time = time(0);
+            number_of_forks = 0;
+        }
             //forking the process here
-            pid = fork();
-            if(pid == -1){
-                perror("");
-                exit(0);
+        pid = fork();
+        if(pid == -1){
+            perror("");
+            exit(0);
+        }
+        //Child process execution 'number_of_forks' is added +1 after each successful fork, this is transferred back to the parent process to make sure the fork
+        //is happening with compliance with the timer (not too fast)
+        if (pid == 0){
+            number_of_forks+=1;
+            if(write(fd[1], &number_of_forks, sizeof(int)) == -1){
+                puts("Can't write to pipe");
             }
-            //Child process execution 'number_of_forks' is added +1 after each successful fork, this is transferred back to the parent process to make sure the fork
-            //is happening with compliance with the timer (not too fast)
-            if (pid == 0){
-                number_of_forks+=1;
-                if(write(fd[1], &number_of_forks, sizeof(int)) == -1){
-                    puts("Can't write to pipe");
-                }
-                execv(command[0],command.data());
-                //if execution fails due to some error, throw a message and kill the parent process with SIGTERM - which should kill the child too
-                perror("Failed");
-                puts("Please use absolute path for your program");
-                kill(getppid(),SIGTERM);
+            execv(command[0],command.data());
+            //if execution fails due to some error, throw a message and kill the parent process with SIGTERM - which should kill the child too
+            perror("Failed");
+            puts("Please use absolute path for your program");
+            kill(getppid(),SIGTERM);
             }
             else{
                 //parent reading from the child and puts it into the 'number_of_forks' variable
